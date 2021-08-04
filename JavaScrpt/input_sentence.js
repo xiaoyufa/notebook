b206d349d191d569acad2849d3d2230f12ffa621
yufa.xiao <1195987323@qq.com>
2021-08-04 09:48:19 +0800

    重置后再连接

--- JavaScrpt/input_sentence.js
+++ JavaScrpt/input_sentence.js
@@ -1,9 +1,12 @@
-
-let a;
-console.log(a);
-a=0;
-console.log(a);
-if(a){
-    console.log('测试boolean的值');
-    console.log(Boolean(null))
+var num = Number(prompt('请输入数字'));
+console.log(num);
+if (isNaN(num)) {
+    alert('输入了非数字')
+} else {
+    if (num > 10) {
+        alert('数字大于10')
+    } else {
+        alert('数字小于10')
+    }
 }
+
